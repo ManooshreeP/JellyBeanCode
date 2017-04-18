26bffb1bd0128ff39ae2bdab5eb629df77bb32b1
ManooshreeP <manooshreepatel@berkeley.edu>
2017-04-17 23:14:15 -0700

    Create test_EstNumJellyBeans.py

---
+++ test_EstNumJellyBeans.py
@@ -0,0 +1,14 @@
+from EstNumJellyBeans import *
+
+num = NumJellyEstimator()
+
+num.set_land_frac_for_sugar(.5)
+num.set_land_frac_for_sugar(2.5)
+
+num.set_world_pop("hello")
+num.set_world_pop(3,000,000)
+num.set_world_pop(10,000,000,000)
+
+set_frac_ppl_loving_pink("three")
+set_frac_ppl_loving_pink("0.3")
+
